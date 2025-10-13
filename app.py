cc222afec57be6638592b32523efa8517fbc4b78
Emmanuel Segun-Lean <34924520+LeanKhan@users.noreply.github.com>
2025-10-13 10:48:12 -0500

    feat: add flask_admin

--- app.py
+++ app.py
@@ -7,14 +7,68 @@ Author: Emmanuel Segun-Lean
 import os
 from flask import Flask, render_template, flash, request, redirect, send_from_directory, url_for
 from werkzeug.utils import secure_filename
+from flask_admin import Admin, expose, BaseView
+from flask_admin.contrib.sqla import ModelView
+from models.story import Tag, db, Story
+
+from flask_admin import Admin
+from wtforms import FileField
 
 UPLOAD_FOLDER = './uploads'
 ALLOWED_EXTENSIONS = {'pptx', 'ppt'}
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///stories.db"
+app.config["SECRET_KEY"] = "supersecretkeyomg"
 
-app.secret_key = b'supersecretkeyomgs'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config["UPLOAD_FOLDER"] = os.path.join(os.getcwd(), "uploads")
+
+app.config["FILE_UPLOAD_FOLDER"] = os.path.join(app.config["UPLOAD_FOLDER"], "files")
+app.config["THUMBNAIL_UPLOAD_FOLDER"] = os.path.join(app.config["UPLOAD_FOLDER"], "thumbnails")
+
+os.makedirs(app.config["FILE_UPLOAD_FOLDER"], exist_ok=True)
+os.makedirs(app.config["THUMBNAIL_UPLOAD_FOLDER"], exist_ok=True)
+
+# set optional bootswatch theme
+app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
+
+db.init_app(app)
+
+class StoryModelView(ModelView):
+    # column_list = ("id", "filename", "title", "tags", "uploaded_at")
+    # form_columns = ("file_upload", "title", "description", "author", "created_at", "tags")
+    
+    form_excluded_columns = ('filename', 'path', 'thumbnail_filename', 'thumbnail_path', 'uploaded_at')
+
+    # Add file upload field    
+    def scaffold_form(self):
+        form_class = super(StoryModelView, self).scaffold_form()
+        form_class.file_upload = FileField("Main File")
+        form_class.thumbnail_upload = FileField("Thumbnail (optional)")
+        return form_class
+
+    # Handle file saving
+    def on_model_change(self, form, model, is_created):
+        file_data = form.file_upload.data
+        if file_data:
+            filename = secure_filename(file_data.filename)
+            save_path = os.path.join(app.config["FILE_UPLOAD_FOLDER"], filename)
+            file_data.save(save_path)
+            model.filename = filename
+            model.path = save_path
+
+        # Handle thumbnail upload
+        thumb_data = form.thumbnail_upload.data
+        if thumb_data:
+            thumb_filename = secure_filename(thumb_data.filename)
+            thumb_path = os.path.join(app.config["THUMBNAIL_UPLOAD_FOLDER"], thumb_filename)
+            thumb_data.save(thumb_path)
+            model.thumbnail_filename = thumb_filename
+            model.thumbnail_path = thumb_path
+
+admin = Admin(app, name="Local CMS", template_mode="bootstrap3")
+admin.add_view(StoryModelView(Story, db.session))
+admin.add_view(ModelView(Tag, db.session))
 
 stories = [
         {
@@ -72,6 +126,9 @@ if __name__ == "__main__":
     # to manage the secret key!
     app.secret_key = 'super secret key'
     app.config['SESSION_TYPE'] = 'filesystem'
+    
+    with app.app_context():
+        db.create_all()
 
     app.debug = True
     app.run()
